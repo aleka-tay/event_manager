6192b04bbcaea5dd5eef691067e2a3912e512cc7
aleka-tay <almarwa.amirtay@gmail.com>
2025-02-28 18:14:18 +0100

    bugfix

--- event_manager.rb
+++ event_manager.rb
@@ -13,6 +13,17 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def clean_phone_number(phone_number)
+  phone_number = phone_number.to_s.gsub(/[^\d]/, '')
+  if phone_number.length == 10
+    phone_number
+  elsif phone_number.length == 11 && phone_number[0] == '1'
+    phone_number[1..10]
+  else
+    'Bad Number'
+  end
+end
+
 def legislator_by_zipcode(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -36,13 +47,26 @@ def save_thank_you_letter(id, form_letter)
     file.puts form_letter
   end
 end
-
+date = Hash.new(0)
+time = Hash.new(0)
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
+  phone_number = clean_phone_number(row[:phone_number])
+  date_and_time = row[:regdate]
+  dates = DateTime.strptime(date_and_time, '%m/%d/%Y %k:%M')
   legislators = legislator_by_zipcode(zipcode)
+
+  d = dates.day
+  t = dates.hour
+  date[:d] += 1
+  time[:t] += 1
   # personal_letter = template_letter.gsub('FIRST_NAME', name)  gsub substitutes first parameter with second one
   # personal_letter.gsub!('LEGISLATORS', legislators)
   form_letter = erb_template.result(binding)
   save_thank_you_letter(id, form_letter)
 end
+
+puts "Most frequent days: #{date.max_by { |k| date[:k] }}"
+puts "Most frequent hour: #{time.max_by { |k| time[:k] }}"
